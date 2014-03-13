292eeeb0104fa303f27a0e5dc08ddc010ac4ed92
Johannes Wachter <johannes.wachter@students.fhv.at>
2014-03-13 16:13:01 +0100

    added basic definitions

---
+++ hw_uart.h
@@ -0,0 +1,17 @@
+/*
+ * Hal: hw_uart.h
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: Mar 13, 2014
+ * Description: 
+ * TODO
+ */
+
+#ifndef HW_UART_H_
+#define HW_UART_H_
+
+/** Registers */
+#define UART_SYSC   (0x54)
+#define UART_SYSS   (0x58)
+
+#endif /* HW_UART_H_ */
