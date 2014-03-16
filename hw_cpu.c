09767c4e3313ec12819e02db3416288373ac791d
Johannes Wachter <johannes.wachter@students.fhv.at>
2014-03-16 15:10:56 +0100

    added cpu definitions

---
+++ hw_cpu.c
@@ -0,0 +1,24 @@
+/*
+ * Hal: hw_cpu.c
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: Mar 16, 2014
+ * Description: 
+ * TODO
+ */
+
+void CPUirqd(void)
+{
+    // Disable IRQ in CPSR
+    asm("    mrs     r0, CPSR\n\t"
+        "    orr     r0, r0, #0x80\n\t"
+        "    msr     CPSR_c, r0");
+}
+
+void CPUirqe(void)
+{
+    // Enable IRQ in CPSR
+    asm("    mrs     r0, CPSR\n\t"
+        "    bic     r0, r0, #0x80\n\t"
+        "    msr     CPSR_c, r0");
+}
