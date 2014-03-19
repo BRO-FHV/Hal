8cd927b62476f9cc408bcd1366b6eeea22ce4591
sla89 <skullsplitter@die-optimisten.net>
2014-03-19 11:28:28 +0100

    merge

--- basic.h
+++ basic.h
@@ -5,7 +5,6 @@
  * Created on: Mar 14, 2014
  * Description: 
  * TODO
-<<<<<<< HEAD
  *
  */
 
@@ -19,28 +18,11 @@
 #define wait(x)	while(x);
 #define reg32r(b, r) (*(volatile uint32_t *)((b)+(r)))
 #define reg32w(b, r, v) (*((volatile uint32_t *)((b)+(r))) = (v))
-<<<<<<< HEAD
-#define reg32m(b, r, v) (*((volatile uint32_t *)((b)+(r))) |= (v))
-#define reg32a(b, r, v) (*((volatile uint32_t *)((b)+(r))) &= (v))
-=======
- */
-
-#ifndef BASIC_H_
-#define BASIC_H_
-
-#define reg32r(b, r) (*(volatile uint32_t *)((b)+(r)))
-#define reg32w(b, r, v) (*((volatile uint32_t *)((b)+(r))) = (v))
-<<<<<<< HEAD
->>>>>>> origin/feature/gpio
-=======
 #define reg32m(b, r, v) (*((volatile uint32_t *)((b)+(r))) |= (v))
 #define reg32a(b, r, v) (*((volatile uint32_t *)((b)+(r))) &= (v))
 #define reg32an(b, r, v) (*((volatile uint32_t *)((b)+(r))) &= ~(v))
 
->>>>>>> origin/feature/gpioLed
-=======
 #define reg32wor(b, r, v) (*((volatile uint32_t *)((b)+(r))) |= (v))
 #define reg32wxor(b, r, v) (*((volatile uint32_t *)((b)+(r))) ^= (v))
->>>>>>> origin/feature/timer
 
 #endif /* BASIC_H_ */
